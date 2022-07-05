c6e80fa7520a7236f9443689e861f8bd3a8ad22b
laquiante <33266194+laquiante@users.noreply.github.com>
2022-07-05 20:49:11 +0200

    Create app.py

---
+++ web/unit-03/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+# Benutzer: cumulus/ON-01/web
+# source bootcamp/bin/activate
+# Dafür sollten dann die API vorbereitet sein
+
+from flask import Flask
+from flask import render_template
+
+import requests
+import urllib3
+
+# nicht schoen aber heute Nachmittag zweckmaessig
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+    nos = []
+    ergebnis = requests.get('https://192.168.200.2:8765/cue_v1/system', auth=('cumulus','Academy123'), verify=False)
+    for key in ergebnis.json():
+      if key == "build":
+        nos.append(ergebnis.json()[key])
+    return nos
+
+app.run(host='::',debug=True)

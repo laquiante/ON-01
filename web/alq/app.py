f157c95b706dce3b073581f9db9507026e289cc2
laquiante <33266194+laquiante@users.noreply.github.com>
2022-08-14 16:12:03 +0200

    Update app.py

--- web/alq/app.py
+++ web/alq/app.py
@@ -21,24 +21,28 @@ projects = [
         "id_feld":   "01",
         "cat":  "base",
         "icon": "/img/thumb_schulung_01.png",
+        "slug": "beginn",
     },
     {
         "name": "Hardware & VX",
         "id_feld":   "02",
         "cat":  "base",
         "icon": "/img/thumb_schulung_02.png",
+        "slug": "geraete",
     },
     {
         "name": "Out-Of-Band",
         "id_feld":   "03",
         "cat":  "base",
         "icon": "/img/thumb_schulung_03.png",
+        "slug": "zugang",
     },
     {
         "name": "NVUE & Co.",
         "id_feld":   "04",
         "cat":  "base",
         "icon": "/img/thumb_schulung_04.png",
+        "slug": "befehle"
     }
 ]
 

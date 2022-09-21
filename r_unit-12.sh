e43bf0280e12f3dbf25af1c6ec9e47b61bdbadb4
laquiante <33266194+laquiante@users.noreply.github.com>
2022-09-21 07:10:13 +0200

    Update r_unit-12.sh

--- r_unit-12.sh
+++ r_unit-12.sh
@@ -1,1 +1,43 @@
+#!/bin/bash
+# ex:ts=4:sw=4:sts=4:et
+# -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+# ALQ, Wittdün 2022
+# 
+# sudo ./r_unit-03
+
+if [ -f /home/cumulus/ON-01/additional.conf ]; then
+        . /home/cumulus/ON-01/additional.conf
+fi
+
+PS3='Unit 12| EVPN-L2. Please enter your choice: '
+options=("move to final unit status" "hands-on leaf11" "hands-on leaf11 and leaf22" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "move to final unit status")
+            echo "configuring final status"
+            ansible-playbook -i /home/cumulus/ON-01/inventory/files/hosts ./unit-12-role/playbooks/main.yaml
+            break
+            ;;
+        "hands-on leaf11")
+            echo "hands-on leaf11"
+            ansible-playbook -i /home/cumulus/ON-01/inventory/files/hosts ./unit-12-role/playbooks/main.yaml
+            ansible-playbook -i /home/cumulus/ON-01/inventory/files/hosts ./unit-12-role/playbooks/l11.yaml
+            break
+            ;;
+        "hands-on leaf11 and spine13")
+            echo "you chose choice hands-on leaf11 and spine13"
+            ansible-playbook -i /home/cumulus/ON-01/inventory/files/hosts ./unit-12-role/playbooks/main.yaml
+            ansible-playbook -i /home/cumulus/ON-01/inventory/files/hosts ./unit-12-role/playbooks/l11.yaml
+            ansible-playbook -i /home/cumulus/ON-01/inventory/files/hosts ./unit-12-role/playbooks/l22.yaml
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
 ansible-playbook -i /home/cumulus/ON-01/inventory/files/hosts ./unit-12-role/playbooks/main.yaml

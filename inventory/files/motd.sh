22af7dbe48cf1cde9b124780397d717712201b7f
laquiante <33266194+laquiante@users.noreply.github.com>
2023-03-22 18:22:57 +0100

    Update motd.sh

--- inventory/files/motd.sh
+++ inventory/files/motd.sh
@@ -36,13 +36,13 @@ echo -e " \e[0;32m
    ##     #######  ##     ##    ##    ######## ########\e[0m
 "
 
-echo -e "You are working now in: \e[0;32m" $DC" \e[0m"
-echo -e "On device:              \e[0;32m" $HOSTNAME" \e[0m"
-echo -e "Operating System:       \e[0;34m" $OS" \e[0m"
-echo -e "Kernel:                 \e[0;34m" $KERNEL" \e[0m"
-echo -e "Uptime:                 " $UP
-echo -e "Managemnt via eth0:     " $IPv4_mgmt
-echo -e "Loopback:               " $IPv4_lo
+echo -e "  You are working now in: \e[0;32m" $DC" \e[0m"
+echo -e "  On device:              \e[0;32m" $HOSTNAME" \e[0m"
+echo -e "  Operating System:       \e[0;34m" $OS" \e[0m"
+echo -e "  Kernel:                 \e[0;34m" $KERNEL" \e[0m"
+echo -e "  Uptime:                 " $UP
+echo -e "  Managemnt via eth0:     " $IPv4_mgmt
+echo -e "  Loopback:               " $IPv4_lo
 
 echo " "
 echo " "
